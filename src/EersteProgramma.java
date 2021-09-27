28d7622e9a6e6740df8d210d6aa3b3275d0d9e6e
Frederik Gailly <Frederik.Gailly@ugent.be>
2021-09-27 11:18:46 +0200

    Demo Intro Les

--- src/EersteProgramma.java
+++ src/EersteProgramma.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class EersteProgramma {
+    public static void main(String[] args) {
+        System.out.println("Hallo iedereen");
+        System.out.println("Geef twee getallen lijn per lijn");
+        System.out.println("en ik zal ze optellen voor jou");
+
+        Scanner input = new Scanner(System.in);
+
+        int getal1 = input.nextInt();
+        int getal2 = input.nextInt();
+
+        int sum = getal1 + getal2;
+
+        System.out.println("Het resultaat is " + sum);
+
+    }
 }
